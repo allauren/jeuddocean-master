5837be9c50676f94dadfd8c7d4e85b76322e6a4a
alexis laurent <alexislaurentprof@gmail.com>
2019-06-20 06:34:20 +0200

    push du projet de grille avant la grand refonte sous format de webapp

--- gridmaker/gridmaker.py
+++ gridmaker/gridmaker.py
@@ -10,14 +10,21 @@ import os, shutil
 import operator
 from sklearn.cluster import KMeans
 
+
+
 def distance_between(p1, p2):
     a = p2[0] - p1[0]
     b = p2[1] - p1[1]
     return np.sqrt((a ** 2) + (b ** 2))
 
+    # fonction qui permet de retrouver la couleur la plus presente dans une zone donnee retour en BGR peut etre faudra
+    # t'il convertir cette valeur en "Couleur" texte afin de faciliter son identification par la suite
 def unique_count_app(a):
     colors, count = np.unique(a.reshape(-1,a.shape[-1]), axis=0, return_counts=True)
     return colors[count.argmax()]
+
+    # cette fonction permet de redefinir une zone d'interet a partir des points qui ont ete determines comme appartenant
+    # au plus grand carre dans l'image et de "redresser" l'image afin d'obtenir une carte qui soit exploitable facilement.
 
 def crop_and_warp(img, crop_rect):
     top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
@@ -32,6 +39,10 @@ def crop_and_warp(img, crop_rect):
     m = cv2.getPerspectiveTransform(src, dst)
     return cv2.warpPerspective(img, m, (int(side), int(side)))
 
+    #cette fonction permet de determiner la position des coins du polygone le plus grand, cette fonctionnalite permet
+    #par la suite de redessiner sur le champ de la camera le polygone qui nous interesse et d'appliquer une regle de proportionnalite
+    #qui determine la position de chaque cercle par rapport a la longieur et largeur de ce polygone trouve.
+
 def find_corners_of_largest_polygon(img):
     contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
@@ -41,6 +52,9 @@ def find_corners_of_largest_polygon(img):
     bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
     top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
     return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
+
+    # cette fonction permet de transformer l'image que nous possedons actuellement en une image noir et blanc afin
+    # par la suite de determiner les contours de la carte ce qui nous permetra de determiner les couleurs presentes sur les cases.
 
 def pre_process_image(img):
     proc = cv2.GaussianBlur(img.copy(), (9, 9), 0)
@@ -52,14 +66,19 @@ def main():
     clean_img = cv2.imread('map.png')
     img = cv2.cvtColor(clean_img, cv2.COLOR_RGB2GRAY)
     processed = pre_process_image(img)
-
-    contours, tresh = cv2.findContours(processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     corners = find_corners_of_largest_polygon(processed)
     cropped = crop_and_warp(clean_img, corners)
-    cv2.imwrite('hope.jpg', cropped)
-    cv2.imwrite('ball.jpg', cropped[350:530, 450:600])
+    # cv2.imwrite('hope.jpg', cropped)
+    # calculs pour determiner la position des cercles par rapport a la taille du carre redessine
+
+    size = cropped.shape[0]
+    height_start = int(size * 400 / 4219)
+    print(height_start)
+
+    cv2.imwrite('ball.jpg', cropped[height_start:530, 450:600])
     cv2.imwrite('ball2.jpg', cropped[530:700, 350:510])
     cv2.imwrite('ball3.jpg', cropped[530:700, 550:700])
+
     val1 = unique_count_app(cropped[350:530, 450:600])
     val2 =unique_count_app(cropped[530:700, 350:510])
     val3 = unique_count_app(cropped[530:700, 550:700])
